3f05f53daec68f6fae04a2105f37998611ca5084
azumber <ahmet.zumberoglu@gmail.com>
2021-11-01 12:32:03 +0300

    flashcards project started

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="max-w-md p-9 rounded-3xl shadow-2xl">
+    <input class="p-2" type="text" placeholder="write a question.."><br>
+    <input class="p-2" type="text" placeholder="write an answer.."><br>
+    <button id="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      SUBMIT
+    </button>
+  </div>
+</template>
+
+<style scoped>
+ .btn {
+    margin: 10px;
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Card />
+</template>
+
+<script>
+export default {}
+</script>
